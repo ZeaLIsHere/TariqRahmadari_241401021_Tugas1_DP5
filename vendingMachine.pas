b37a7dd913876d84c19f86b99de7fa77adf4b166
ZeaL <tariq.rahmadari23@gmail.com>
2024-09-29 16:00:36 +0700

    ubah nama variable

--- vendingMachine.pas
+++ vendingMachine.pas
@@ -3,30 +3,30 @@ uses crt;
 
 var
     NP : integer;
-    SN : string;
+    PN : string;
 
     begin
         clrscr;
         write('Pilih Nomor Produk: ');
         readln(NP);
         case (NP) of
-            001 : SN := 'Pemsi';
-            002 : SN := 'Comca Comla';
-            003 : SN := 'Semprite';
-            004 : SN := 'Famta';
-            005 : SN := 'Big Comla';
-            006 : SN := 'Sumsu Bemruang';
-            007 : SN := 'Sumsu Bemdera';
-            008 : SN := 'Kompi Kemnangan';
-            009 : SN := 'Chimomry';
-            010 : SN := 'Somsis Imdomaret';
+            001 : PN := 'Pemsi';
+            002 : PN := 'Comca Comla';
+            003 : PN := 'Semprite';
+            004 : PN := 'Famta';
+            005 : PN := 'Big Comla';
+            006 : PN := 'Sumsu Bemruang';
+            007 : PN := 'Sumsu Bemdera';
+            008 : PN := 'Kompi Kemnangan';
+            009 : PN := 'Chimomry';
+            010 : PN := 'Somsis Imdomaret';
         else
             begin
                 writeln('Nomor Produk Tidak Diketahui');
             end;
         end;
     writeln;
-    writeln('Produk yang anda beli adalah: ',SN);
+    writeln('Produk yang anda beli adalah: ',PN);
     writeln('Terimakasih Sudah Membeli:)');
     readln;
 end.
